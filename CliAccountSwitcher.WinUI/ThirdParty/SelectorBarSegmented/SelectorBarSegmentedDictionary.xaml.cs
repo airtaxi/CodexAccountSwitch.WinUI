aeacbc0489db77595f765dc265334852d1b3bfcc
Howon Lee <kck4156@kagamine-rin.com>
2026-09-06 22:48:10 +0900

    fix: replace SelectorBarSegmented with ported fork to prevent layout cycle crash

---
+++ CliAccountSwitcher.WinUI/ThirdParty/SelectorBarSegmented/SelectorBarSegmentedDictionary.xaml.cs
@@ -0,0 +1,8 @@
+using Microsoft.UI.Xaml;
+
+namespace CliAccountSwitcher.WinUI.ThirdParty.SelectorBarSegmented;
+
+public sealed partial class SelectorBarSegmentedDictionary : ResourceDictionary
+{
+    public SelectorBarSegmentedDictionary() => InitializeComponent();
+}
